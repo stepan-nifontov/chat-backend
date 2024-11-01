74136228711cf63d00a290731cb7fb6190dbe600
Stepan Nifontov <snifontov@samokat.ru>
2024-11-01 23:16:07 +0500

    add api

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello from the backend!');
 });
 
